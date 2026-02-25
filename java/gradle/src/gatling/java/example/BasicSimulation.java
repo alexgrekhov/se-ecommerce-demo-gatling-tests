f2a02074af67db4f6fa650fb10a3f535ce567d05
Alex Grekhov <galexalex801@gmail.com>
2026-02-25 17:00:51 +0200

    feat: add Jenkins pipeline, docker-compose, and update Gatling simulations

--- java/gradle/src/gatling/java/example/BasicSimulation.java
+++ java/gradle/src/gatling/java/example/BasicSimulation.java
@@ -8,30 +8,22 @@ import io.gatling.javaapi.http.*;
 
 public class BasicSimulation extends Simulation {
 
-  // Load VU count from system properties
-  // Reference: https://docs.gatling.io/guides/passing-parameters/
-  private static final int vu = Integer.getInteger("vu", 1);
-
   // Define HTTP configuration
-  // Reference: https://docs.gatling.io/reference/script/protocols/http/protocol/
-  private static final HttpProtocolBuilder httpProtocol =
-      http.baseUrl("https://api-ecomm.gatling.io")
-          .acceptHeader("application/json")
-          .userAgentHeader(
-              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+  // Reference: https://docs.gatling.io/reference/script/http/protocol/
+  HttpProtocolBuilder httpProtocol =
+          http.baseUrl("https://api-ecomm.gatling.io")
+                  .acceptHeader("application/json")
+                  .userAgentHeader(
+                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
 
   // Define scenario
-  // Reference: https://docs.gatling.io/reference/script/core/scenario/
-  private static final ScenarioBuilder scenario =
-      scenario("Scenario").exec(http("Session").get("/session"));
-
-  // Define assertions
-  // Reference: https://docs.gatling.io/reference/script/core/assertions/
-  private static final Assertion assertion = global().failedRequests().count().lt(1L);
+  // Reference: https://docs.gatling.io/concepts/scenario/
+  ScenarioBuilder scenario =
+          scenario("Scenario").exec(http("Session").get("/session"));
 
   // Define injection profile and execute the test
-  // Reference: https://docs.gatling.io/reference/script/core/injection/
+  // Reference:  https://docs.gatling.io/concepts/injection/
   {
-    setUp(scenario.injectOpen(atOnceUsers(vu))).assertions(assertion).protocols(httpProtocol);
+    setUp(scenario.injectOpen(constantUsersPerSec(2).during(60))).protocols(httpProtocol);
   }
 }
